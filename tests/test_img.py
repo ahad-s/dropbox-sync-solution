88c7c126416c7fe1c74d71d51d090459f42dcef8
Ahad S. <ahad_cool@hotmail.co.uk>
2015-01-15 20:30:35 -0500

    Changed dir skeleton

---
+++ tests/test_img.py
@@ -0,0 +1,7 @@
+def test_open():
+	assert open("../dropbox-sync-solution-images/dropbox_icon.ico","r")
+	assert open("../dropbox-sync-solution-images/push_to_dropbox.gif","r")
+	assert open("../dropbox-sync-solution-images/sync_from_dropbox.gif","r")
+	assert open("../dropbox-sync-solution-images/windows_file_error.png","r")
+
+test_open()
